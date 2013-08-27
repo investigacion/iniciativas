f12be32282296ca1fcdf7770c871d7ce2adb2bea
Matthew Caruana Galizia <mattcg@gmail.com>
2013-08-27 16:40:09 -0600

    Trim lines

--- scripts/data/iniciativas.js
+++ scripts/data/iniciativas.js
@@ -9,21 +9,26 @@ exports.convert = function(csvPath, jsonPath) {
 	fs.writeFileSync(jsonPath, JSON.stringify(csv.parse(fs.readFileSync(csvPath, {
 		encoding: 'utf8'
 	})).reduce(function(p, c, i) {
-		var d;
+		var d, t, url;
+
+		t = function(s) {
+			return s && s.trim();
+		};
 
 		d = {
 			'número': parseInt(c['Número de Iniciativa'], 10),
-			proponente: c.Proponente,
-			tipo: c['Tipo de Iniciativa'],
-			asunto: c.Asunto,
-			tema: c.Tema,
-			subtema: c.Subtema,
+			proponente: c.Proponente.trim(),
+			tipo: c['Tipo de Iniciativa'].trim(),
+			asunto: c.Asunto.trim(),
+			tema: c.Tema.trim(),
+			subtema: c.Subtema.trim(),
 			'año': parseInt(c['Año de recibido'], 10),
-			'trámite': c['Trámite efectuado']
+			'trámite': c['Trámite efectuado'].trim()
 		};
 
-		if (c['URL ley de república']) {
-			d.url = c['URL ley de república'];
+		url = t(c['URL ley de república']);
+		if (url) {
+			d.url = url;
 		}
 
 		p.push(d);
